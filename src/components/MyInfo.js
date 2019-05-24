94701f001bd0ebb62236abba83ca1b315d6eb5be
stephenyoon84 <stephenyoon84@gmail.com>
2019-05-24 10:20:46 -0400

    add default value for input field

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -8,15 +8,17 @@ class MyInfo extends Component {
   render() {
     if (!!localStorage.token) {
       if (this.props.user) {
+        const cUser = this.props.user
+        console.log(cUser)
         return (
           <div>
             <Segment placeholder>
               <Grid columns={3} relaxed='very' centered>
                 <Grid.Column>
                   <Form>
-                    <Form.Input icon='user' iconPosition='left' label="Name" name="name" />
-                    <Form.Input icon='mail' iconPosition='left' label='email' name='email' />
-                    <Form.Input icon='phone' iconPosition='left' label='Phone Number' name='phoneNumber' />
+                    <Form.Input icon='user' iconPosition='left' label="Name" name="name" defaultValue={cUser.name} />
+                    <Form.Input icon='mail' iconPosition='left' label='email' name='email' defaultValue={cUser.email}/>
+                    <Form.Input icon='phone' iconPosition='left' label='Phone Number' name='phoneNumber' defaultValue={cUser.phone_number}/>
                     <Form.Input icon='lock' iconPosition='left' label='Current Password' type='password' name='currentPassword' />
                     <Form.Input icon='lock' iconPosition='left' label='New Password' type='password' name='newPassword' />
                     <Form.Input icon='lock' iconPosition='left' label='Password Confirmation' type='password' name='passwordConfirmation' />
