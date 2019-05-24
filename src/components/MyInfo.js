1fe652c37a7a44b00ffcc91702a1bde77dbf1e32
stephenyoon84 <stephenyoon84@gmail.com>
2019-05-24 10:07:37 -0400

    add form input field

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Button, Form, Segment, Grid} from 'semantic-ui-react';
 import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 // import {URL} from '../redux/actionCreators';
@@ -9,7 +10,22 @@ class MyInfo extends Component {
       if (this.props.user) {
         return (
           <div>
-            {this.props.user.user_type}
+            <Segment placeholder>
+              <Grid columns={3} relaxed='very' centered>
+                <Grid.Column>
+                  <Form>
+                    <Form.Input icon='user' iconPosition='left' label="Name" name="name" />
+                    <Form.Input icon='mail' iconPosition='left' label='email' name='email' />
+                    <Form.Input icon='phone' iconPosition='left' label='Phone Number' name='phoneNumber' />
+                    <Form.Input icon='lock' iconPosition='left' label='Current Password' type='password' name='currentPassword' />
+                    <Form.Input icon='lock' iconPosition='left' label='New Password' type='password' name='newPassword' />
+                    <Form.Input icon='lock' iconPosition='left' label='Password Confirmation' type='password' name='passwordConfirmation' />
+
+                    <Button type='submit' content='Submit' primary />
+                  </Form>
+                </Grid.Column>
+              </Grid>
+            </Segment>
           </div>
         )
       } else {
