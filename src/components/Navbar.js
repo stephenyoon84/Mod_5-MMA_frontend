1ab111bcf50e0ff8860238ad2d8a2c6248eef6a6
stephenyoon84 <stephenyoon84@gmail.com>
2019-05-30 12:58:01 -0400

    add style to nav bar when hover

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,18 +8,18 @@ class Navbar extends Component {
 
   render() {
     return (
-      <Menu>
+      <Menu className='navbar'>
         <Menu.Item name='Home' className='logo home'><Link to='/'><img style={{width: 150, height: 50}} src='http://www.kumcgw.org/wp-content/uploads/2013/09/KUMCGW-logo_Final-Korean_small.png' alt=''/></Link></Menu.Item>
-        <Menu.Item name='NewForm'><Link to='/newmember' >New Member<br/>새가족</Link></Menu.Item>
+        <Menu.Item name='NewForm' className='navbtn'><Link to='/newmember' >New Member<br/>새가족</Link></Menu.Item>
         <Fragment>
           { (this.props.user && this.props.user.user_type !== 'new_user') || !!localStorage.token ? (
             <>
               {
                 this.props.user && this.props.user.user_type !== 'new_user' ? (
                   <>
-                    <Menu.Item name='AllMembers'><Link to='/members'>Manage New Members<br/>새가족 관리</Link></Menu.Item>
+                    <Menu.Item name='AllMembers' className='navbtn'><Link to='/members'>Manage New Members<br/>새가족 관리</Link></Menu.Item>
                     {/*<Menu.Item name='SmallGroups'><Link to='/smallgroups'>Small Groups</Link></Menu.Item>*/}
-                    <Menu.Item name='SmallGroupList'><Link to='/smallgroups'>Small Groups<br/>속 관리</Link></Menu.Item>
+                    <Menu.Item name='SmallGroupList' className='navbtn'><Link to='/smallgroups'>Small Groups<br/>속 관리</Link></Menu.Item>
                     {/*<Menu.Item name='Orientation'><Link to='/orientation'>Orientation(optional)</Link></Menu.Item>*/}
                   </>
                 ) : (
@@ -28,13 +28,13 @@ class Navbar extends Component {
               }
               {
                 this.props.user && this.props.user.user_type === 'admin' ? (
-                  <Menu.Item name='ManageUsers'><Link to='/users'>Manage Users<br/>새가족팀관리</Link></Menu.Item>
+                  <Menu.Item name='ManageUsers' className='navbtn'><Link to='/users'>Manage Users<br/>새가족팀관리</Link></Menu.Item>
                 ) : (
                   null
                 )
               }
-              <Menu.Item name="MyInfo" id='rightmyinfo' className='right myinfo'><Link to='/myinfo'>My Info<br/>내 정보</Link></Menu.Item>
-              <Menu.Item name="LogOut" id='logout' className='right Logout' onClick={() => {
+              <Menu.Item name="MyInfo" id='rightmyinfo' className='right myinfo navbtn'><Link to='/myinfo'>My Info<br/>내 정보</Link></Menu.Item>
+              <Menu.Item name="LogOut" id='logout' className='right Logout navbtn' onClick={() => {
                   localStorage.clear()
                   this.props.logoutUser()
                   this.props.clearMembers()
@@ -44,7 +44,7 @@ class Navbar extends Component {
                 }}>Log Out</Menu.Item>
               </>
           ) : (
-            <Menu.Item name="LogIn" className='right login' ><Link to="/login">Log In</Link></Menu.Item>
+            <Menu.Item name="LogIn" className='right login navbtn' ><Link to="/login">Log In</Link></Menu.Item>
           )}
         </Fragment>
       </Menu>
