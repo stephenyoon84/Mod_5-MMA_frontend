547ec13f2468b222e942a1b230b0b09ca8bda1e9
stephenyoon84 <stephenyoon84@gmail.com>
2019-05-30 14:08:37 -0400

    change welcome message

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class Welcome extends Component {
   render() {
-    return <h1 id='welcome'>Welcome to KUMC<br/>와싱톤 한인교회에 오신것을 환영합니다.</h1>
+    return <h1 id='welcome'>Welcome to KUMC of GW<br/>와싱톤 한인교회에 오신것을 환영합니다.</h1>
   }
 }
 
