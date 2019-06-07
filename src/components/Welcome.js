3ba1dd13322e9d3e76de937860d41fdcd410667c
stephenyoon84 <stephenyoon84@gmail.com>
2019-06-07 16:57:55 -0400

    new plan

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,12 @@ import React, {Component} from 'react';
 
 class Welcome extends Component {
   render() {
-    return <h1 id='welcome'>Welcome to KUMC of GW<br/>와싱톤 한인교회에 오신것을 환영합니다.</h1>
+    return (
+      <div>
+        <h1 id='welcome'>Welcome to KUMC of GW<br/>와싱톤 한인교회에 오신것을 환영합니다.</h1>
+        <div>Link to register or register form</div>
+      </div>
+    )
   }
 }
 
